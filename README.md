0a17a2995b5b71644d92c5b5920c15bd338bc705
Szymon Okulowski <szymon.okulowski@gmail.com>
2022-01-07 17:15:48 +0100

    add footer and finish project

---
+++ README.md
@@ -0,0 +1,33 @@
+# Frontend Mentor - Loopstudios landing page solution
+
+This is a solution to the [Loopstudios landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/loopstudios-landing-page-N88J5Onjw). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: https://github.com/szymonOk/frontendmentor_loopstudios
+- Live Site URL: https://szymonok.github.io/frontendmentor_loopstudios/
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- BEM & SASS
+- JavaScript
+
+### Author
+
+Github: https://github.com/szymonOk
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const hamBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav__list-mobile')
 const listItems = navMobile.querySelectorAll('a')
 const logoLink = document.querySelector('.nav__logo-item')
+const navBar = document.querySelector('.nav')
+const footerYear = document.querySelector('.footer__text-year')
 
 const handleBurger = () => {
 	hamBtn.classList.toggle('is-active')
@@ -14,5 +16,22 @@ const closeNav = () => {
 	hamBtn.classList.remove('is-active')
 }
 
+// SCROLL DARK BG
+const handleScroll = () => {
+	if (window.scrollY >= 657) {
+		navBar.classList.add('nav-scroll')
+	} else {
+		navBar.classList.remove('nav-scroll')
+	}
+}
+
+// YEAR
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
 hamBtn.addEventListener('click', handleBurger)
 logoLink.addEventListener('click', closeNav)
+window.addEventListener('scroll', handleScroll)
